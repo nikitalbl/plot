5718f9e700487cbf068423aaaace1e3ba65bae43
nikitalbl <90863712+nikitalbl@users.noreply.github.com>
2021-09-26 00:24:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var myPlot = document.getElementById('myDiv');
-let rand = Array.from({length: 1000001}, () => Math.random() * 1000001);
+let rand = Array.from({length: 1001}, () => Math.random() * 1001);
 var xy = new Float32Array(rand);
 
 data = [{ xy: xy,  type: 'pointcloud' }];
