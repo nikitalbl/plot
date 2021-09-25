dbf9d8754da9b76d4b7ed610f2f95ba2dbc973dc
nikitalbl <90863712+nikitalbl@users.noreply.github.com>
2021-09-26 00:32:48 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var myPlot = document.getElementById('myDiv');
-let rand = Array.from({length: 1001}, () => Math.random() * 1001);
-var xy = new Float32Array(rand);
-
-data = [{ xy: xy,  type: 'pointcloud' }];
-layout = { };
-
-Plotly.newPlot('myDiv', data, layout);
